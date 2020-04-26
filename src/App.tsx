335712991abc974e5c9f2607ea0af71d56776f20
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-04-26 11:32:09 +0700

    feat: add about components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Home from './components/home';
+import About from './components/about';
 
 export default (): any => <div>
   <h1>App Here</h1>
   <Home title="My App" />
+  <About />
 </div>;
---
+++ src/components/about/index.tsx
@@ -0,0 +1,1 @@
+export default ({}: any): any => <h1>About Page</h1>;
